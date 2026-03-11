9e440d2168ca73e5d289d4ed3415521615130c68
ogheneruemuemmanuel2-tech <ogheneruemuemmanuel2@gmail.com>
2026-03-11 06:56:46 +0100

    Rename detect_pump to detect_top and update logic

--- strategy .py
+++ strategy .py
@@ -3,7 +3,8 @@ from mexc_api import get_price
 
 price_history = []
 
-def detect_pump(symbol):
+
+def detect_top(symbol):
 
     global price_history
 
@@ -11,22 +12,28 @@ def detect_pump(symbol):
 
     price_history.append(price)
 
-    # keep only last 10 prices
-    if len(price_history) > 10:
+    # keep only last 20 prices
+    if len(price_history) > 20:
         price_history.pop(0)
 
-    if len(price_history) < 10:
+    if len(price_history) < 6:
         return False
 
-    first_price = price_history[0]
-    last_price = price_history[-1]
+    p1 = price_history[-6]
+    p2 = price_history[-5]
+    p3 = price_history[-4]
+    p4 = price_history[-3]
+    p5 = price_history[-2]
+    p6 = price_history[-1]
 
-    pump_percent = ((last_price - first_price) / first_price) * 100
+    # detect strong pump then slowdown
+    pump = (p4 - p1) / p1 * 100
 
-    print("Pump check:", round(pump_percent,2), "%")
+    # detect top rejection
+    if pump > 20 and p6 < p5:
 
-    # if price pumped 25% in few seconds
-    if pump_percent > 25:
+        print("⚠️ Possible TOP detected")
+
         return True
 
     return False
